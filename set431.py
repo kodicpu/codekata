6b4104be67dab7d2bf7dc53db3e2129264b17e97
kodicpu <dhanushdkm@gmail.com>
2019-08-01 22:16:59 +0530

    Update set431.py

--- set431.py
+++ set431.py
@@ -2,6 +2,8 @@ strdata=input()
 length=len(strdata)
 count=0
 for i in range(length):
-    if strdata[i].isalpha():
+    if strdata[i].isspace():
+        continue
+    else:
         count+=1
 print (count)
