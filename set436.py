fa40240aa83e8f12730060e914af095532f65885
kodicpu <dhanushdkm@gmail.com>
2019-08-02 11:56:08 +0530

    Update set436.py

--- set436.py
+++ set436.py
@@ -1,5 +1,5 @@
 x,count = input(),0
 for i in x:
-  if (ord(i) not in range(65,90)) and (ord(i) not in range(97,123)) and (ord(i)!=32):
+  if (ord(i) not in range(65,90)) and (ord(i) not in range(97,123)) and (ord(i)!=32) and (ord(i) not in range(47,58)):
     count+=1
 print(count)
